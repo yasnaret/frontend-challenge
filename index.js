2bb1f97f68ac4b5fc26b756981624d94c438244d
yasnaret <yasnaret.soto@gmail.com>
2018-10-28 19:05:40 -0300

    webpack config status:ok

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const app =document.getElementById('app');
+const holamundo=<h1>Hola Mundo!!!</h1>
+
+ReactDom.render(holamundo,app);
