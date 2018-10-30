83f7253b30021cc6b8cff18eacece756121a966d
yasnaret <yasnaret.soto@gmail.com>
2018-10-30 14:06:18 -0300

    responsive masonry grid status:ok

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Images from './src/components/Images'
+import Images  from './src/components/Images';
+import './src/css/style.css'
 
+let brakePoints = [350, 500, 750];
 const app =document.getElementById('app');
 
 
-render(<Images/>,app);
+render(<Images brakePoints={brakePoints}/>,app);
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import ReactTooltip from 'react-tooltip'
+import Masonry from './Masonry'
+
 
 
 export default class Images extends Component {
@@ -7,37 +11,61 @@ export default class Images extends Component {
     super();
     this.state = {
       pictures: [],
+      addPage: 1,
+      hasMoreItems: false,
     };
+
+    this.buildImages();
   }
 
-  componentDidMount() {
+  buildImages() {
     //from https://www.flickr.com/services/api/explore/flickr.photos.search
-    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f45b96e80316446649d82d3d211ce5a8&tags=LADY&format=json&nojsoncallback=1&auth_token=72157697097041790-55c586651e0289e4&api_sig=949b7cc45eb111969d4757ede2740aff')
-      .then(response => {
-        return response.json()
-      })
-      .then(function (j) {
-
-        let picArray = j.photos.photo.map(pic => {
-        let srcPath = `https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`
-          return ( 
-          <img alt="lady" src={srcPath} key= {pic.id}></img>
-          )
-        })
-        this.setState(
-          {pictures: picArray}
-        )
-      }.bind(this))
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=967c78a908c3f74879ddf240240f35fa&tags=woman&sort=+relevance&extras=description+%2Ctags%2Cdate_upload%2C+date_taken%2C+owner_name&per_page=40&page=${this.state.page}&format=json&nojsoncallback=1`)
+      .then(res => res.json())
+      .then(j => {
+        let infoState=this.state.pictures
+        j.photos.photo.map(pic => {
+        infoState.push(pic);
+        });
+        this.setState({ 
+          addPage: this.state.addPage + 1, 
+          addImgs: true });
+      });
   }
 
   render(){
+    
+    return (
+			<div className="container">
+				<div className="masonry-container">
+					<p>ReactJS Responsive| Masonry Grid | Webpack | API Flickr</p>
 
-    return ( 
-
-    <div className ="root">
-        {this.state.pictures}
-    </div>
-
-    );
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.buildImages.bind(this)}
+            hasMore={this.state.hasMoreItems}
+            loader={<div className="loader">Loading wait...</div>}>
+					<Masonry brakePoints={this.props.brakePoints}>
+						{this.state.pictures.map((pic, id) => {
+							return (
+                  <div className="tile">
+                    <img alt="lady"
+                     src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`}
+                     key={pic.id}
+                     data-for='soclose' data-multiline={true} data-tip={`TITLE: ${pic.title},  TAGS: ${pic.tags}`}
+                     data-event='click'/>
+		              <ReactTooltip/>
+                  </div>
+							) 
+						})}
+					</Masonry>
+          </InfiniteScroll>
+				</div>
+			</div>
+		)
   }
 }
+
+
+
+
